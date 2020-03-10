1637989354ef2fcde8c2d3b2f017467a4f40ed1a
masterUNG <yosssup2212@hotmail.com>
2020-03-10 15:25:17 +0700

    ProjectSuccess10March

--- lib/container/show_map.dart
+++ lib/container/show_map.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ShowMap extends StatefulWidget {
   @override
@@ -7,10 +7,43 @@ class ShowMap extends StatefulWidget {
 }
 
 class _ShowMapState extends State<ShowMap> {
+  //field
+  static LatLng gloCenter = LatLng(13.880496, 100.504845);
+  CameraPosition cameraPosition = CameraPosition(
+    target: gloCenter,
+    zoom: 16.0,
+  );
+
+  //method
+
+  Marker gloMaker() {
+    return Marker(
+      infoWindow: InfoWindow(
+        title: 'สำนักงานสลากกินแบ่งรัฐบาล',
+        snippet: '235 ถ.สนามบินน้ำ จ.นนทบุรี',
+      ),
+      markerId: MarkerId('glo'),
+      position: gloCenter,
+    );
+  }
+
+  Set<Marker> setMarker() {
+    return <Marker>[
+      gloMaker(),
+    ].toSet();
+  }
+
+  Widget showGoogleMap() {
+    return GoogleMap(
+      markers: setMarker(),
+      initialCameraPosition: cameraPosition,
+      mapType: MapType.normal,
+      onMapCreated: (GoogleMapController controller) {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('show map');
-      
-    
+    return showGoogleMap();
   }
 }
